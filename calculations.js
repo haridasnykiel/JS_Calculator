07c201913bcdd6f7c54a78703283c8c4a3f1491f
Haridas <haridasnykiel@gmail.com>
2018-01-30 22:09:15 +0000

    Added some of the functionality for calculating numbers

--- calculations.js
+++ calculations.js
@@ -1,22 +1,51 @@
 $(function(event) {
   console.log('DOM is ready');
 
+  var calculation = [];
+  var number = "";
   $('.buttons').on('click',function() {
     var checkClass = $( this ).hasClass( "operator" );
     if(checkClass) {
       output("&nbsp;" + $( this ).val() + "&nbsp;");
+
     } else {
       output($( this ).val());
     }
-    
+    function addNumToArr() {
+      calculation.push(number);
+      number = "";
+    }
+    if($( this ).val() === '×') {
+      addNumToArr();
+      calculation.push('*');
+    } else if($( this ).val() === '÷') {
+      addNumToArr();
+      calculation.push('/');
+    } else if($( this ).val() === '+') {
+      addNumToArr();
+      calculation.push('+');
+    } else if($( this ).val() === '-') {
+      addNumToArr();
+      calculation.push('-');
+    } else {
+      number += $( this ).val();
+    }
+    console.log(calculation);
   });
 
   function output(value) {
     $('#display').append(value);
   }
 
+  $('#equals').click(function(){
+    var joined = calculation.join('');
+    console.log(joined);
+  });
+
   $('#clear-button').click(function(){
     $('#display').empty();
   });
+
+
 
 });
